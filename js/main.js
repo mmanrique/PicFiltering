3c73e5069e9035dffab5a545004c3e800e24aeb2
Manuel Manrique <memo881@gmail.com>
2012-07-26 22:52:58 -0500

    Adding Require.js,Backbone, jQuery and Underscore

---
+++ js/main.js
@@ -0,0 +1,25 @@
+require.config({
+	shim: {
+	    'underscore': {
+	        exports: '_'
+	    },
+
+	    'backbone': {
+	        deps: ['underscore', 'jquery'],
+	        exports: 'Backbone'
+	    },
+
+	},
+	paths: {
+		jquery: 'libs/jquery/jquery-lib',
+		underscore: 'libs/underscore/underscore-lib',
+		backbone: 'libs/backbone/backbone-lib'
+	}
+});
+/*
+Require es una funcion que toma, un array y un callback que se va a ejecutar luego
+The names of each of the values in the first arrays are the names of the js files to import
+*/
+define(['jquery', 'backbone'], function(){
+	
+});
